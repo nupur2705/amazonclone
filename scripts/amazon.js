247b340f9b2560969b62b42ac1a2c2135359bb1a
nupur_032 <nupursatpathy1@gmail.com>
2023-12-29 17:56:39 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -74,7 +74,14 @@ matchingItem=item;
       quantity:1
     });
    }
-    
-    console.log(cart);
+   let cartQuantity=0;
+   cart.forEach((item)=>{
+cartQuantity+=item.quantity;
+   });
+
+   document.querySelector('.js-cart-quantity')
+   .innerHTML=cartQuantity;
+   
+  
   });
 });
