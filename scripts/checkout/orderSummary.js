df9b4d44a16153999284e5bbc03997b910c7807f
nupur_032 <nupursatpathy1@gmail.com>
2024-02-16 23:07:37 +0530

    first commit

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -5,9 +5,6 @@ import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 import { deliveryOptions ,getDeliveryOption} from '../../data/deliveryoptions.js';
 import { renderPaymentSummary } from './paymentSummary.js';
 
-const today=dayjs();
-const deliveryDate=today.add(7,'days');
-console.log(deliveryDate.format('dddd,MMMM D'));
 
 
 
